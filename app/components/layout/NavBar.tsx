03db990df61e5dd75fb5e69d358c7e8aae9d4eba
Tony Yu <tony.kwangjong.yu@gmail.com>
2022-11-08 13:40:39 -0800

    partial styling for navbar and add tailwind classes - test 1

--- app/components/layout/NavBar.tsx
+++ app/components/layout/NavBar.tsx
@@ -1,21 +1,20 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function NavBar() {
   return (
-    <header className='flex items-center justify-between bg-indigo-500 w-full h-20'>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link href='/'>Home</Link>
-            </li>
-            <li>
-              {/* add links here */}
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <header className="flex items-center justify-between bg-indigo-500 w-full h-20 p-4 ">
+      <Link href="/" className=" hover:text-white text-4xl font-bold">
+        FitnessApp
+      </Link>
+      <nav>
+        <ul className="hover:text-white text-2xl list-none m-0 p-0 flex items-baseline ">
+          <li className="ml-12">
+            <Link href="/">Home</Link>
+          </li>
+          <li>{/* add links here */}</li>
+        </ul>
+      </nav>
     </header>
-  )
+  );
 }
